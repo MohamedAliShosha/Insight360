477d8c6911e2ee0678edd425541dd76665fd3742
Mohamed Ali <a74498722@gmail.com>
2025-04-03 21:09:00 -0700

    Add color constants for consistent theming

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryBlue = Color(0xff001F3F);
+const kWhite = Colors.white;
+const kOrange = Color(0xffFFA500);
+const kLightGrey = Color(0xff989898);
+const kTeal = Color(0xff008080);
+const kBlack = Colors.black;
