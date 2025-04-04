87aad956d1fa84c0b67d080d4800b6d2e928517f
Mohamed Ali <a74498722@gmail.com>
2025-04-03 21:16:34 -0700

    Add SplashViewBody with background color and update SplashView structure

--- lib/Features/splash/presentation/views/splash_view.dart
+++ lib/Features/splash/presentation/views/splash_view.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/constants.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: kPrimaryBlue,
+    );
   }
 }
