374f7555ab1af4e02eab182c3717ad54ba5a0a95
Mohamed Ali <a74498722@gmail.com>
2025-04-03 22:31:11 -0700

    Add logo and text to SplashViewBody with Google Fonts integration

--- lib/Features/splash/presentation/views/splash_view.dart
+++ lib/Features/splash/presentation/views/splash_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:news_app/constants.dart';
 
 class SplashView extends StatelessWidget {
@@ -19,6 +20,43 @@ class SplashViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPrimaryBlue,
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'Assets/Images/logo 1 (1).png',
+              width: 120, // Adjust size as needed
+              height: 120,
+              fit: BoxFit.contain,
+            ),
+            // const SizedBox(width: 8), // Space between logo and text
+            RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'INSIGHT ',
+                    style: GoogleFonts.inter(
+                      fontSize: 31,
+                      fontWeight: FontWeight.w600,
+                      color: kOrange,
+                    ),
+                  ),
+                  // Text span is
+                  TextSpan(
+                    text: '360°',
+                    style: GoogleFonts.inter(
+                      fontSize: 31,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white, // Adjusted to match design
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
