bb4e24e2b4804a65769fc3c779d321b4693d1966
Mohamed Ali <a74498722@gmail.com>
2025-04-04 21:54:47 -0700

    Move SplashViewBody to a separate file and implement sliding animation in SplashView

--- lib/Features/splash/presentation/views/splash_view.dart
+++ lib/Features/splash/presentation/views/splash_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:news_app/Features/Auth/presentation/views/sign_in_view.dart';
-import 'package:news_app/constants.dart';
+import 'package:news_app/Features/splash/presentation/widgets/splash_view_body.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -60,63 +59,6 @@ class _SplashViewState extends State<SplashView>
   Widget build(BuildContext context) {
     return Scaffold(
       body: SplashViewBody(slidingAnimation: slidingAnimation),
-    );
-  }
-}
-
-class SplashViewBody extends StatelessWidget {
-  final Animation<Offset> slidingAnimation;
-
-  const SplashViewBody({super.key, required this.slidingAnimation});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kPrimaryBlue,
-      body: Center(
-        child: AnimatedBuilder(
-          animation: slidingAnimation,
-          builder: (context, child) {
-            return SlideTransition(
-              position: slidingAnimation,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'Assets/Images/logo 1 (1).png',
-                    width: 120,
-                    height: 120,
-                    fit: BoxFit.contain,
-                  ),
-                  const SizedBox(width: 8),
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: 'INSIGHT ',
-                          style: GoogleFonts.inter(
-                            fontSize: 31,
-                            fontWeight: FontWeight.w600,
-                            color: kOrange,
-                          ),
-                        ),
-                        TextSpan(
-                          text: '360°',
-                          style: GoogleFonts.inter(
-                            fontSize: 31,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
-      ),
     );
   }
 }
---
+++ lib/Features/splash/presentation/widgets/splash_view_body.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/constants.dart';
+
+class SplashViewBody extends StatelessWidget {
+  final Animation<Offset> slidingAnimation;
+
+  const SplashViewBody({super.key, required this.slidingAnimation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryBlue,
+      body: Center(
+        child: AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context, child) {
+            return SlideTransition(
+              position: slidingAnimation,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'Assets/Images/logo 1 (1).png',
+                    width: 120,
+                    height: 120,
+                    fit: BoxFit.contain,
+                  ),
+                  const SizedBox(width: 8),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'INSIGHT ',
+                          style: GoogleFonts.inter(
+                            fontSize: 31,
+                            fontWeight: FontWeight.w600,
+                            color: kOrange,
+                          ),
+                        ),
+                        TextSpan(
+                          text: '360°',
+                          style: GoogleFonts.inter(
+                            fontSize: 31,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
