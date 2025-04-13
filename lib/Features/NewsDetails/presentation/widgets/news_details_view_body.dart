d14733c8c2638e83c3a5e3c288e1806d1761e46e
Mohamed Ali <a74498722@gmail.com>
2025-04-13 09:47:53 -0700

    refactor(news_details_view_body): replace ListView with SingleChildScrollView and update image styling
    Replaced ListView with SingleChildScrollView to improve scrolling behavior and updated the image styling with a Container to include borderRadius and fit properties. Also, added a date text field with a custom color for better UI consistency.

--- lib/Features/NewsDetails/presentation/widgets/news_details_view_body.dart
+++ lib/Features/NewsDetails/presentation/widgets/news_details_view_body.dart
@@ -1,49 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/constants.dart';
 
 class NewsDetailsViewBody extends StatelessWidget {
   const NewsDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return SingleChildScrollView(
       physics: const BouncingScrollPhysics(),
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 50),
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * .4,
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 30),
-                  child: Image(
-                    image: AssetImage('Assets/Images/health.png'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 50),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * .4,
+              child:  Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Container(
+                  
+                  height: 420,
+                  width: 431,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    image: DecorationImage(
+                      image: AssetImage('Assets/Images/science.png'),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: Text(
-                maxLines: 3,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-                'There is no description available for this book',
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18.93),
-              ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
+              maxLines: 3, //
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+              'There is no description available for this book',
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18.93),
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: Text(
-                'There is no description available for this book',
-                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 10.26),
-              ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
+              'April 12, 2022',
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 13.25, color: kLightGrey),
             ),
-          ],
-        ),
-      ],
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
+              'The Remaining of the description',
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 10.26),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
