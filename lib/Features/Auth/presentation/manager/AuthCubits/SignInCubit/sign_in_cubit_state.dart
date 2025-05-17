e7beb03cb0fdba83e4152009a7a8599b5149a572
Mohamed Ali <a74498722@gmail.com>
2025-05-17 07:48:25 -0700

    feat(auth): add SignInCubitState classes for managing sign-in states

--- lib/Features/Auth/presentation/manager/AuthCubits/SignInCubit/sign_in_cubit_state.dart
+++ lib/Features/Auth/presentation/manager/AuthCubits/SignInCubit/sign_in_cubit_state.dart
@@ -3,3 +3,17 @@ part of 'sign_in_cubit_cubit.dart';
 abstract class SignInCubitState {}
 
 final class SignInCubitInitial extends SignInCubitState {}
+
+final class SignInLoading extends SignInCubitState {}
+
+final class SignInSuccess extends SignInCubitState {
+  final String message;
+
+  SignInSuccess(this.message);
+}
+
+final class SignInError extends SignInCubitState {
+  final String errorMessage;
+
+  SignInError(this.errorMessage);
+}
