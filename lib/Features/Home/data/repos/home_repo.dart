f5491bb96497d086335c58cd57660b324f6d3b5b
Mohamed Ali <a74498722@gmail.com>
2025-05-18 14:35:59 -0700

    fix(home): correct type name from Failure to Failures in HomeRepo interface

--- lib/Features/Home/data/repos/home_repo.dart
+++ lib/Features/Home/data/repos/home_repo.dart
@@ -3,5 +3,5 @@ import 'package:news_app/Features/Home/data/models/news_model/news_model.dart';
 import 'package:news_app/core/errors/failures.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, List<NewsModel>>> getToHeadLines();
+  Future<Either<Failures, List<NewsModel>>> getToHeadLines();
 }
