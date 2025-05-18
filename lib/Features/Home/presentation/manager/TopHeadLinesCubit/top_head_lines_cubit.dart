d575e0fc3386eacb251036f55cafca2de4af2786
Mohamed Ali <a74498722@gmail.com>
2025-05-18 15:27:46 -0700

    chore(top_head_lines_cubit): remove unnecessary blank line in imports

--- lib/Features/Home/presentation/manager/TopHeadLinesCubit/top_head_lines_cubit.dart
+++ lib/Features/Home/presentation/manager/TopHeadLinesCubit/top_head_lines_cubit.dart
@@ -2,7 +2,6 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/Features/Home/data/models/news_model/news_model.dart';
 import 'package:news_app/Features/Home/data/repos/home_repo.dart';
-
 part 'top_head_lines_state.dart';
 
 class TopHeadLinesCubit extends Cubit<TopHeadLinesState> {
