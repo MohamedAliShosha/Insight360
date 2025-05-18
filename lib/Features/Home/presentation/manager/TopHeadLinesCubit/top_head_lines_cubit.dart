f035120e3eb2b1bd7b50d588960fac4c4984d5e3
Mohamed Ali <a74498722@gmail.com>
2025-05-18 15:27:22 -0700

    feat(home): implement TopHeadLinesCubit with dependency injection for HomeRepo

--- lib/Features/Home/presentation/manager/TopHeadLinesCubit/top_head_lines_cubit.dart
+++ lib/Features/Home/presentation/manager/TopHeadLinesCubit/top_head_lines_cubit.dart
@@ -1,9 +1,29 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/Features/Home/data/models/news_model/news_model.dart';
+import 'package:news_app/Features/Home/data/repos/home_repo.dart';
 
 part 'top_head_lines_state.dart';
 
 class TopHeadLinesCubit extends Cubit<TopHeadLinesState> {
-  TopHeadLinesCubit() : super(TopHeadLinesInitial());
+  TopHeadLinesCubit(this.homeRepo) : super(TopHeadLinesInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> getTopHeadLines({
+    required String apiKey,
+    required String country,
+    required String category,
+  }) async {
+    emit(TopHeadLinesLoading());
+
+    var result = await homeRepo.getToHeadLines(
+        apiKey: apiKey, country: country, category: category);
+
+    result.fold((failure) {
+      emit(TopHeadLinesError(errorMessage: failure.errorMessage));
+    }, (news) {
+      emit(TopHeadLinesSuccess(news: news));
+    });
+  }
 }
