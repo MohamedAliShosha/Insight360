6b13859b20ba5fa92742053f0e32b4b69eea7bf9
Mohamed Ali <a74498722@gmail.com>
2025-05-21 08:06:35 -0700

    feat(home_view): remove unused imports to clean up HomeView code

--- lib/Features/Home/presentation/views/home_view.dart
+++ lib/Features/Home/presentation/views/home_view.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:news_app/Features/Home/data/repos/home_repo_implement.dart';
-import 'package:news_app/Features/Home/presentation/manager/TopHeadLinesCubit/top_head_lines_cubit.dart';
 import 'package:news_app/Features/Home/presentation/widgets/home_view_body.dart';
-import 'package:news_app/constants.dart';
-import 'package:news_app/core/utils/service_locator.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
