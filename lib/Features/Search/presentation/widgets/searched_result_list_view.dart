819ed40e8a38d852511578d887088d32a2a31d6a
Mohamed Ali <a74498722@gmail.com>
2025-05-22 15:07:58 -0700

    feat(searched_result_list_view): enhance article item interaction with gesture detection and navigation

--- lib/Features/Search/presentation/widgets/searched_result_list_view.dart
+++ lib/Features/Search/presentation/widgets/searched_result_list_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import 'package:news_app/Features/Search/presentation/manager/SearchCubit/search_cubit.dart';
 import 'package:news_app/Features/Search/presentation/widgets/search_shimmer_item.dart';
+import 'package:news_app/core/utils/app_router.dart';
 import 'package:news_app/core/utils/colors_manager.dart';
 import 'package:news_app/core/widgets/custom_error_widget.dart';
 
@@ -20,67 +22,75 @@ class SearchedResultListView extends StatelessWidget {
             physics: const BouncingScrollPhysics(),
             itemCount: state.articles.length,
             itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 8),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 100,
-                      width: 140,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: state.articles[index].urlToImage != null &&
-                                  state.articles[index].urlToImage!.isNotEmpty
-                              ? NetworkImage(state.articles[index].urlToImage!)
-                              : const AssetImage('Assets/Images/general.png')
-                                  as ImageProvider,
+              return GestureDetector(
+                onTap: () {
+                  GoRouter.of(context).push(AppRouter.kNewsDetailsView,
+                      extra: state.articles[index]);
+                },
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 4, vertical: 8),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 140,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: state.articles[index].urlToImage != null &&
+                                    state.articles[index].urlToImage!.isNotEmpty
+                                ? NetworkImage(
+                                    state.articles[index].urlToImage!)
+                                : const AssetImage('Assets/Images/general.png')
+                                    as ImageProvider,
+                          ),
+                          borderRadius: BorderRadius.circular(8),
                         ),
-                        borderRadius: BorderRadius.circular(8),
                       ),
-                    ),
-                    const SizedBox(width: 8),
-                    Expanded(
-                      // تستخدم هنا بشكل صحيح داخل Row
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            state.articles[index].title ?? '',
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                              color: ColorsManager.kWhite,
-                              fontWeight: FontWeight.w600,
-                              fontSize: 16,
+                      const SizedBox(width: 8),
+                      Expanded(
+                        // تستخدم هنا بشكل صحيح داخل Row
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              state.articles[index].title ?? '',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                color: ColorsManager.kWhite,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16,
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            DateFormat('yyyy-MM-dd').format(DateTime.parse(
-                                state.articles[index].publishedAt ?? '')),
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w400,
-                              fontSize: 14,
-                              color: ColorsManager.kLightGrey,
+                            const SizedBox(height: 8),
+                            Text(
+                              DateFormat('yyyy-MM-dd').format(DateTime.parse(
+                                  state.articles[index].publishedAt ?? '')),
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 14,
+                                color: ColorsManager.kLightGrey,
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            state.articles[index].source!.name ?? '',
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                              color: ColorsManager.kWhite,
+                            const SizedBox(height: 8),
+                            Text(
+                              state.articles[index].source!.name ?? '',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: ColorsManager.kWhite,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               );
             },
