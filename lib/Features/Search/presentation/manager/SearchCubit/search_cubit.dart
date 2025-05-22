9f5971318d27e51ed0427257e16daa524e2cc370
Mohamed Ali <a74498722@gmail.com>
2025-05-22 11:14:38 -0700

    feat(search_cubit): implement SearchCubit and SearchState for managing search functionality

---
+++ lib/Features/Search/presentation/manager/SearchCubit/search_cubit.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/Features/Home/data/models/news_model/article.dart';
+import 'package:news_app/Features/Search/data/repos/search_repo.dart';
+
+part 'search_state.dart';
+
+class SearchCubit extends Cubit<SearchState> {
+  SearchCubit(this.searchRepo) : super(SearchInitial());
+
+  final SearchRepo searchRepo;
+  Future<void> getSearchedNews(
+      {required String apiKey,
+      required String category,
+      required String country}) async {
+    emit(SearchLoading());
+
+    var result = await searchRepo.getSearchedNews(
+        apiKey: apiKey, country: country, category: category);
+
+    result.fold((failuire) {
+      emit(
+        SearchFailure(errorMessage: failuire.errorMessage),
+      );
+    }, (article) {
+      emit(
+        SearchSuccess(articles: article),
+      );
+    });
+  }
+}
