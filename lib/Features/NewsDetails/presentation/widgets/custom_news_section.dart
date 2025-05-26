cefbbb0c6d0a9008e80f90ec2639ef1fad906316
Mohamed Ali <a74498722@gmail.com>
2025-05-26 14:51:58 -0700

    feat(custom_button): add CustomButton widget for reusable button styling and functionality feat(custom_news_section): implement BooksAction widget with CustomButton for article actions feat(styles): define text styles for consistent typography across the app

---
+++ lib/Features/NewsDetails/presentation/widgets/custom_news_section.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/Features/Home/data/models/news_model/article.dart';
+import 'package:news_app/Features/NewsDetails/presentation/widgets/custom_button.dart';
+import 'package:news_app/core/functions/launch_url_function.dart';
+
+class BooksAction extends StatelessWidget {
+  const BooksAction({super.key, required this.articleModel});
+  final Article articleModel;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        children: [
+          const Expanded(
+            child: CustomButton(
+              text: '19.99\$',
+              backgroundColor: Colors.white,
+              textColor: Colors.black,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16),
+                bottomLeft: Radius.circular(16),
+              ),
+            ),
+          ),
+          Expanded(
+            child: CustomButton(
+              onPressed: () {
+                launchCustomUrl(context, articleModel.url);
+              },
+              fontSize: 16,
+              text: getText(articleModel),
+              backgroundColor: const Color(0xffEF8262),
+              textColor: Colors.white,
+              borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(12),
+                bottomRight: Radius.circular(12),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String getText(Article articleModel) {
+    if (articleModel.url == null) {
+      return 'Not Available';
+    } else {
+      return 'Preview';
+    }
+  }
+}
