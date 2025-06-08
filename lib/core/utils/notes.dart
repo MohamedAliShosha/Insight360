09770132d283ac2ddb8ca67639cb3bd9273210ed
Mohamed Ali <a74498722@gmail.com>
2025-06-08 09:54:17 -0700

    fix(notes): correct typos in comments and improve clarity

--- lib/core/utils/notes.dart
+++ lib/core/utils/notes.dart
@@ -1,4 +1,4 @@
-// Need to make the pjone text field to accept only numbers => Done
+// Need to make the phone text field to accept only numbers => Done
 // check the logic of sign up => Done
 // Create the cubit of the sign in => Done
 
@@ -7,6 +7,9 @@
 /*
 1. Create the repo class
 2. Importing the DartZ package to provide the Either type for handling success and failure cases.
-3. Importing the Failures class to define the types of failures that can occur. => Failure class is Copied freom another project as It ready to use
+3. Importing the Failures class to define the types of failures that can occur. => Failure class is Copied from another project as It ready to use
 4. Importing the NewsModel class to define the data model for the news articles.
+
+** I need to adjust the text Styles in SignUp view
+
 */
