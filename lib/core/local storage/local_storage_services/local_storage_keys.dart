f3c728204ee7209c5f2ded9cdd296851d0dd2ce5
Mohamed Ali <a74498722@gmail.com>
2025-06-08 19:13:25 -0700

    feat(local_storage_keys): add phoneNumber key for local storage

--- lib/core/local storage/local_storage_services/local_storage_keys.dart
+++ lib/core/local storage/local_storage_services/local_storage_keys.dart
@@ -1,4 +1,5 @@
 abstract class LocalStorageKeys {
   static const String userName = 'user_name';
   static const String userEmail = 'user_email';
+  static const String phoneNumber = 'phone_number';
 }
