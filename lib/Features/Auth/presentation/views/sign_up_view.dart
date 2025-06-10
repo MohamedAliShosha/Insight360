b532ab5386d9a65d4d3d3f06646565a39da69b19
Mohamed Ali <a74498722@gmail.com>
2025-06-10 09:07:04 -0700

    feat(sign_up_view): refactor to use MultiBlocProvider for improved state management

--- lib/Features/Auth/presentation/views/sign_up_view.dart
+++ lib/Features/Auth/presentation/views/sign_up_view.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/Features/Auth/presentation/manager/AuthCubits/SignUpCubit/sign_up_cubit.dart';
 import 'package:news_app/Features/Auth/presentation/widgets/sign_up_view_body.dart';
-
+import 'package:news_app/core/local%20storage/local_storage_cubit/local_storage_cubit.dart';
+import 'package:news_app/core/local%20storage/local_storage_services/local_storage_service.dart';
 import 'package:news_app/core/utils/colors_manager.dart';
 
 class SignUpView extends StatelessWidget {
@@ -12,8 +13,17 @@ class SignUpView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorsManager.kWhite,
-      body: BlocProvider(
-        create: (context) => SignUpCubit(),
+      body: MultiBlocProvider(
+        providers: [
+          BlocProvider<SignUpCubit>(
+            create: (context) => SignUpCubit(),
+          ),
+          BlocProvider<LocalStorageCubit>(
+            create: (context) => LocalStorageCubit(
+              LocalStorageService(),
+            ),
+          ),
+        ],
         child: const SignUpViewBody(),
       ),
     );
