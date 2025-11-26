51793e94c890bd783cdd2e5e3156e935a0f4504a
Mohamed Ali <a74498722@gmail.com>
2025-11-26 10:39:40 -0800

    refactor: Replace HomeViewBodyBlocBuilder with HomeViewBodyBlocConsumer in HomeViewBody

--- lib/Features/Home/presentation/widgets/home_view_body.dart
+++ lib/Features/Home/presentation/widgets/home_view_body.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/Features/Home/presentation/widgets/home_view_body_bloc_builder.dart';
+import 'package:news_app/Features/Home/presentation/widgets/home_view_body_bloc_consumer.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const HomeViewBodyBlocBuilder();
+    return const HomeViewBodyBlocConsumer();
   }
 }
