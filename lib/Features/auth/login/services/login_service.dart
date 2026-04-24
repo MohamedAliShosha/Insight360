3372a0753477bc02edd27e3e7a30c05c2fdbe727
mohamed <mohamedaliaboshosha927@gmail.com>
2026-04-23 18:19:54 -0700

    refactor(login_service): update API endpoint handling and improve error logging

--- lib/Features/auth/login/services/login_service.dart
+++ lib/Features/auth/login/services/login_service.dart
@@ -1,6 +1,5 @@
-import 'package:retrofit/http.dart';
-
-import '../../../../core/utils/constants.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:news_app/core/utils/api_endpoints.dart';
 
 import '../data/models/login_request_body.dart';
 import '../data/models/login_response.dart';
@@ -8,10 +7,10 @@ import 'package:dio/dio.dart';
 
 part 'login_service.g.dart';
 
-@RestApi(baseUrl: Constants.registrationBaseUrl)
+@RestApi()
 abstract class LoginService {
   factory LoginService(Dio dio, {String? baseUrl}) = _LoginService;
 
-  @POST('/auth/login')
+  @POST(ApiEndpoints.login)
   Future<LoginResponseModel> login(@Body() LoginRequestBody loginRequestBody);
 }
