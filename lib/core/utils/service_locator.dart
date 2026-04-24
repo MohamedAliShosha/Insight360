be21fdc49b9ca3fccba89ce8ef698ba874563854
mohamed <mohamedaliaboshosha927@gmail.com>
2026-04-23 18:26:32 -0700

    refactor(service_locator): remove ApiService and integrate NewsService and HomeService

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,13 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-import 'package:news_app/Features/Home/data/repos/home_repo_implement.dart';
-import 'package:news_app/Features/Search/data/repos/search_repo_implement.dart';
-import 'package:news_app/Features/auth/login/data/repos/login_repo_implement.dart';
-import 'package:news_app/Features/auth/login/services/login_service.dart';
-import 'package:news_app/Features/auth/sign_up/data/repos/sign_up_repo.dart';
-import 'package:news_app/Features/auth/sign_up/data/repos/sign_up_repo_implement.dart';
-import 'package:news_app/Features/auth/sign_up/services/sign_up_service.dart';
-import 'package:news_app/core/utils/api_service.dart';
+import '../../Features/Home/data/repos/home_repo_implement.dart';
+import '../../Features/Home/data/services/home_service.dart';
+import '../../Features/Home/data/services/news_service.dart';
+import '../../Features/Search/data/repos/search_repo_implement.dart';
+import '../../Features/auth/login/data/repos/login_repo_implement.dart';
+import '../../Features/auth/login/services/login_service.dart';
+import '../../Features/auth/sign_up/data/repos/sign_up_repo.dart';
+import '../../Features/auth/sign_up/data/repos/sign_up_repo_implement.dart';
+import '../../Features/auth/sign_up/services/sign_up_service.dart';
+import 'constants.dart';
 
 final getIt = GetIt.instance; // Singleton instance of GetIt
 
@@ -15,23 +17,29 @@ void setUpServiceLocator() {
   getIt.registerLazySingleton<Dio>(
     () => Dio(),
   );
-  getIt.registerLazySingleton<ApiService>(
-    () => ApiService(getIt<Dio>()),
+
+  getIt.registerLazySingleton<NewsService>(
+    () => NewsService(getIt<Dio>(), baseUrl: Constants.newsBaseUrl),
+  );
+  getIt.registerLazySingleton<HomeService>(
+    () => HomeService(getIt<Dio>(), baseUrl: Constants.newsBaseUrl),
   );
 
   getIt.registerSingleton<HomeRepoImplement>(
     HomeRepoImplement(
-      getIt<ApiService>(),
+      getIt<HomeService>(),
     ),
   );
   getIt.registerSingleton<LoginService>(
     LoginService(
       getIt<Dio>(),
+      baseUrl: Constants.registrationBaseUrl,
     ),
   );
   getIt.registerSingleton<SignUpService>(
     SignUpService(
       getIt<Dio>(),
+      baseUrl: Constants.registrationBaseUrl,
     ),
   );
   getIt.registerSingleton<SignUpRepo>(
@@ -48,7 +56,7 @@ void setUpServiceLocator() {
 
   getIt.registerSingleton<SearchRepoImplement>(
     SearchRepoImplement(
-      getIt<ApiService>(),
+      getIt<NewsService>(),
     ),
   );
 }
